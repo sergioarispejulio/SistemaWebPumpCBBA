6d382cc426a30f09011c8d65258e588c3ea8f03a
sergioarispejulio <sergio_arispe@hotmail.com>
2014-11-22 19:31:31 -0300

    FIX login mistake
    Solucionado el erro de que si se logueaban mal, aparecia un error

--- SocialPIU/app/controllers/sessions_controller.rb
+++ SocialPIU/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user
       redirect_back_or_to root_url, :notice => "Logged in!"
     else
-      flash.now.alert = "Email or password was invalid."
+      redirect_to login_path
     end
   end
 
